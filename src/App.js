8ae99e41c7be309761df23874e74b337b0b53e96
Rakib <16103030@iubat.edu>
2019-09-08 00:15:03 +0600

    Setup Done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import firebase from 'firebase'
 import Home from './component/Home'
 import React, {Component} from 'react';
-import {Route, Switch, Redirect} from 'react-router-dom'
+import {Route, Switch, Redirect, Link} from 'react-router-dom'
 import fireInitialize from './config/fire'
 
 class App extends Component{
@@ -13,7 +13,9 @@ class App extends Component{
       otp: false,
       otpSuccess: false,
       userCode: '',
-      forgotBtn: true
+      forgotBtn: true,
+      errors: '',
+      loginSuccess: false
     }
   }
 
@@ -41,8 +43,20 @@ class App extends Component{
 
   handleChange = (e) => {
     const {value, name} = e.target
+    const {password, confirmPass} = this.state
     this.setState({
       [name]: value
+    }, () => {
+      if (name === 'confirmPass' && password !== value){
+        localStorage.setItem('password', password)
+        this.setState({
+          errors: 'Password Did Not Match'
+        })
+      } else {
+        this.setState({
+          errors: ''
+        })
+      }
     })
   }
 
@@ -54,40 +68,58 @@ class App extends Component{
       })
       console.log('Success OTP:', result)
     }).catch(error => {
-      console.log('Error!!: ', error)
+      this.setState({
+        errors: 'Wrong Code!'
+      })
     });
   }
 
+  login = () => {
+    if(this.state.confirmPass === localStorage.getItem('password')){
+      this.setState({
+        loginSuccess: true
+      })
+    }
+  }
+
   render(){
-    const {otp, otpSuccess, userCode, forgotBtn, confirmPass, password} = this.state
+    const {otp, otpSuccess, userCode, forgotBtn, confirmPass, password, errors, loginSuccess, passwordCheck} = this.state
     return (
       <div className="App">
         <Switch>
-          <Route path='/' render={
-            () => otpSuccess ? <Home /> : <Redirect
+          <Route path='/home' render={
+            () => otpSuccess && loginSuccess ? <Home /> : <Redirect
               to={{
-                pathname:'/login'
+                pathname:'/'
               }}
             />
           }/>
-          <Route path='/login' Component={Home} />
+
+          <Route exact path='/' Component={App} />
         </Switch>
+        {(!otpSuccess || !loginSuccess) && <>
         {forgotBtn && !otp ? <h2 className='fotgotPass' onClick={() => {this.setState({forgotBtn: false})}}>Forgot Password?</h2> : <>
           {!otp && 
           <div className='loginApp'>
             <input onChange={this.handleChange} name='phoneNumber' type='tel' value={this.state.phoneNumber} autoFocus={true} placeholder='Enter your phone number' />
-            <button onClick={this.phoneValidation}>Login</button>
+            <button onClick={this.phoneValidation}>Get Code</button>
             <div id="recaptcha-container"></div>
           </div>}
-          {otp &&
+          {!otpSuccess ? otp &&
           <>
-            <div className='loginApp'>
+            <div className='loginApp2'>
+              {errors && <p>{errors}</p>}
               <input type='text' value={userCode} onChange={this.handleChange} name='userCode' autoFocus={true} placeholder='Enter the code'/>
               <input type='password' value={password} onChange={this.handleChange} name='password' placeholder='Enter Password'/>
               <input type='password' value={confirmPass} onChange={this.handleChange} name='confirmPass' placeholder='Confirm Password'/>
               <button onClick={this.useOtp}>Reset</button>
             </div>
-          </>}
+          </> : <div className='loginApp2'>
+              <input type='text' placeholder='Enter Your Name'/>
+              <input type='password' value={passwordCheck} onChange={this.handleChange} name='passwordCheck' placeholder='Enter Password'/>
+              <button onClick={this.login}><Link to='/home'>Log In </Link></button>
+          </div>}
+        </>}
         </>}
       </div>
     );
--- src/component/Home.js
+++ src/component/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
     return(
-        <div>
+        <div className='welcomeBack'>
             <h1><span>&#128515;</span></h1>
             <h1>Welcome Back!</h1>
         </div>
