d0a963fec41257c9a84ab9c7a1276178d71737f6
Rakib <16103030@iubat.edu>
2019-09-07 22:51:00 +0600

    Firebase Phonenumber Auth Setup Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase'
+import Home from './component/Home'
+import React, {Component} from 'react';
+import {Route, Switch, Redirect} from 'react-router-dom'
+import fireInitialize from './config/fire'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      phoneNumber: '',
+      otp: false,
+      otpSuccess: false,
+      userCode: '',
+      forgotBtn: true
+    }
+  }
+
+  phoneValidation = () => {
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
+      'size': 'invisible',
+      'callback': function(response) {
+      }
+    });
+    let phoneNumber = this.state.phoneNumber
+    let appVerifier = window.recaptchaVerifier
+    fireInitialize.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+        .then(confirmationResult => {
+          if(confirmationResult){
+            this.setState({
+              otp: true
+            })
+          }
+          window.confirmationResult = confirmationResult
+        })
+        .catch(function (error) {
+          console.log('Error!!: ', error)
+        });
+  }
+
+  handleChange = (e) => {
+    const {value, name} = e.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  useOtp = () => {
+    window.confirmationResult.confirm(this.state.userCode)
+    .then(result => {
+      this.setState({
+        otpSuccess: true
+      })
+      console.log('Success OTP:', result)
+    }).catch(error => {
+      console.log('Error!!: ', error)
+    });
+  }
+
+  render(){
+    const {otp, otpSuccess, userCode, forgotBtn} = this.state
+    return (
+      <div className="App">
+        <Switch>
+          <Route path='/' render={
+            () => otpSuccess ? <Home /> : <Redirect
+              to={{
+                pathname:'/login'
+              }}
+            />
+          }/>
+          <Route path='/login' Component={Home} />
+        </Switch>
+        {forgotBtn && !otp ? <>
+          {!otp && 
+          <div className='loginApp'>
+            <input onChange={this.handleChange} name='phoneNumber' type='tel' value={this.state.phoneNumber} placeholder='Enter your phone number' />
+            <button onClick={this.phoneValidation}>Login</button>
+            <div id="recaptcha-container"></div>
+          </div>}
+          {otp &&
+          <>
+            <input type='text' value={userCode} onChange={this.handleChange} name='userCode' />
+            <button onClick={this.useOtp}>Reset</button>
+          </>}
+        </> : <h2 onClick={() => {this.setState({forgotBtn: false})}}>Forgot Password?</h2>}
+      </div>
+    );
+  }
 }
 
 export default App;
